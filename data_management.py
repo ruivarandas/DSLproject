056351063eeb0bdb4b9f6711b7e671b00964f2a5
bbg-fct <bb.goncalves@campus.fct.unl.pt>
2020-12-26 11:40:48 +0000

    resize val data fixed

--- data_management.py
+++ data_management.py
@@ -109,13 +109,13 @@ class DataPreparation:
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]),
             'val': transforms.Compose([
-                transforms.Resize(224),
+                transforms.Resize((224,224)),
         #         transforms.CenterCrop(800),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]),
             'test': transforms.Compose([
-                transforms.Resize(224),
+                transforms.Resize((224,224)),
         #         transforms.CenterCrop(800),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
