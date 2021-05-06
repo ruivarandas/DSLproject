006dde360f365ee05f3fb1470ff329996e571d45
ruivarandas <48528346+ruivarandas@users.noreply.github.com>
2021-05-06 16:47:37 +0100

    Add Histogram results

--- class_balance.py
+++ class_balance.py
@@ -11,20 +11,15 @@ def configurations(path):
 def get_class_balance():
     custom_labels = {"abnormal": ["A", "a", "J", "S", "V", "E", "F", "P", "/", "f", "Q"], "normal": ["N", "L", "R", "e", "j"]}
     normal, abnormal = 0, 0
-    print(f'.{sep}Figures{sep}raw_figures{sep}')
-    for folder, files, _ in walk(f'.{sep}Figures{sep}raw_figures{sep}'):
-
+    print(f'.{sep}data{sep}raw_figures{sep}')
+    for folder, files, _ in walk(f'.{sep}data{sep}raw_figures{sep}'):
         for f in files:
-            print("2")
-            for folder1, files1, signals in walk(join(f'.{sep}Figures{sep}raw_figures{sep}', f)):
-                print("3")
+            for folder1, files1, signals in walk(join(f'.{sep}data{sep}raw_figures{sep}', f)):
                 for signal in signals:
-                    print("4")
                     if '.txt' in signal:
-                        f_ = join(join(f'.{sep}Figures{sep}raw_figures{sep}', f), signal)
+                        f_ = join(join(f'.{sep}data{sep}raw_figures{sep}', f), signal)
                         labels = np.loadtxt(f_, dtype=np.object)[1:, 1]
                         for label in labels:
-                            print("5")
                             if label in custom_labels['normal']:
                                 normal += 1
                             elif label in custom_labels['abnormal']:
